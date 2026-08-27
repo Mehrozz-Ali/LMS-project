f9271701726e24e5e98bb6d7198523ef418c294d
Mehrozz-Ali <mehrozali912@gmail.com>
2026-08-27 22:53:05 +0500

    Navbar &  Hero section almost complete

---
+++ client/app/page.tsx
@@ -0,0 +1,25 @@
+'use client';
+import React, { FC, useState } from 'react';
+import Heading from './utils/Heading';
+import Header from './components/Header';
+import Hero from './components/Route/Hero'
+
+
+interface Props { }
+
+
+
+const Page: FC<Props> = (props) => {
+
+  const [open, setOpen] = useState(false);
+  const [activeItem, setActiveItem] = useState(0);
+  return (
+    <div>
+      <Heading title="E-Learning" description="ELearning is a platform for online learning" keywords="Programming,MERN,Redux " />
+      <Header open={open} setOpen={setOpen} activeItem={activeItem} />
+      <Hero />
+    </div>
+  )
+}
+
+export default Page;
