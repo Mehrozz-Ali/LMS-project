4e973a6a92027baff35495cd807c70b2bf566dca
Mehrozz-Ali <mehrozali912@gmail.com>
2026-09-01 18:51:10 +0500

    Get all users,courses,manage team

--- client/redux/features/courses/coursesApi.ts
+++ client/redux/features/courses/coursesApi.ts
@@ -12,11 +12,18 @@ export const courseApi = apiSlice.injectEndpoints({
                 credentials: "include" as const,
             }),
         }),
+        getAllCourses: builder.query({
+            query: () => ({
+                url: "get-courses",
+                method: "GET",
+                credentials: "include" as const,
+            })
+        })
     }),
 });
 
 
-export const { useCreateCourseMutation } = courseApi;
+export const { useCreateCourseMutation, useGetAllCoursesQuery } = courseApi;
 
 
 
