afb5e4e9d24f111be5551c2d3105081b544a51b3
Mehrozz-Ali <mehrozali912@gmail.com>
2026-09-03 23:07:19 +0500

    faq part is in progress

--- client/app/components/Admin/Customization/EditFaq.tsx
+++ client/app/components/Admin/Customization/EditFaq.tsx
@@ -1,15 +1,33 @@
 import { useGetHeroDataQuery } from '@/redux/features/layout/layoutApi'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 type Props = {}
 
 const EditFaq = (props: Props) => {
 
     const { data, isLoading } = useGetHeroDataQuery("FAQ", { refetchOnMountOrArgChange: true })
+    const [questions, setQuestions] = useState<any[]>([]);
+
+
+    useEffect(() => {
+        if (data) {
+            setQuestions(data?.layout?.faq);
+        }
+
+    }, [data]);
+
+
+
 
 
     return (
-        <div>EditFaq</div>
+        <div className="w-[92%] md:w-[80%] m-auto mt-[120px]">
+            <div className="mt-12">
+                <dl className="space-y-6">
+
+                </dl>
+            </div>
+        </div>
     )
 }
 
