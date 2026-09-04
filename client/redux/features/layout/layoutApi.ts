a5f466e6f7a73a65e496974cd85560076f9c8036
Mehrozz-Ali <mehrozali912@gmail.com>
2026-09-04 12:27:08 +0500

    faq part is completed

--- client/redux/features/layout/layoutApi.ts
+++ client/redux/features/layout/layoutApi.ts
@@ -14,7 +14,7 @@ export const layoutApi = apiSlice.injectEndpoints({
         }),
         editLayout: builder.mutation({
             query: ({ type, image, title, subTitle, faq, categories }) => ({
-                url: `edit-layout`,
+                url: "edit-layout",
                 body: {
                     type,
                     image,
